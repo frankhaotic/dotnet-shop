e60f07a4f9c67771ef69c45013220b27c13ddd3f
Frankie Flood <fsfloody@gmail.com>
2021-06-08 00:10:36 +0100

    add CORS support to the api

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,13 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
 
         }
 
@@ -50,6 +57,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();   
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
