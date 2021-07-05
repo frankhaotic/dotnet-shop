1b0ec20f820a84c2ab780f1cc47d082f2db50566
Frankie Flood <fsfloody@gmail.com>
2021-07-05 17:58:32 +0100

    install the toastr package to add toast notifications to show the user when they make a 400 or 401 error

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -8,16 +8,27 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastr: ToastrService) {}
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(req).pipe(
       catchError(error => {
         if(error) {
+          if(error.status === 400) {
+            if(error.error.errors){
+              throw error.error;
+            } else {
+              this.toastr.error(error.error.message, error.error.statusCode);
+            }
+          }
+          if(error.status === 401) {
+            this.toastr.error(error.error.message, error.error.statusCode);
+          }
           if(error.status === 404) {
             this.router.navigateByUrl('/not-found')
           }
