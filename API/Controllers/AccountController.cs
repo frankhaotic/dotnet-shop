0ca2ce3c2c265c562668b8dca514f44be2faa576
Frankie Flood <fsfloody@gmail.com>
2021-07-14 18:54:35 +0100

    allow for put requests to be made to update user information

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using API.Dtos;
 using API.Errors;
 using API.Extensions;
+using AutoMapper;
 using Core.Entities.Identity;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -16,12 +17,15 @@ namespace API.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
 
         public AccountController(UserManager<AppUser> userManager,
-            SignInManager<AppUser> signInManager, ITokenService tokenService)
+            SignInManager<AppUser> signInManager, ITokenService tokenService,
+            IMapper mapper)
         {
             _signInManager = signInManager;
             _tokenService = tokenService;
+            _mapper = mapper;
             _userManager = userManager;
         }
 
@@ -47,14 +51,28 @@ namespace API.Controllers
 
         [Authorize]
         [HttpGet("address")]
-        public async Task<ActionResult<Address>> GetUserAddress()
-        {
-            
+        public async Task<ActionResult<AddressDto>> GetUserAddress()
+        {            
             var user =  await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(User);
 
-            return user.Address;
+            return _mapper.Map<Address, AddressDto>(user.Address);
         }
         
+        [Authorize]
+        [HttpPut("address")]
+        public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto address)
+        {
+            var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(User);
+
+            user.Address = _mapper.Map<AddressDto, Address>(address);
+
+            var result = await _userManager.UpdateAsync(user);
+
+            if(result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
+
+            return BadRequest("Problem updating the user");
+        }
+
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
