a8604f8d4820b9c1425dae0b0bee97400047180d
Frankie Flood <fsfloody@gmail.com>
2021-07-17 13:47:26 +0100

    implementing the new orders with items and ordering spec to the OrderService

--- Core/Interfaces/IOrderService.cs
+++ Core/Interfaces/IOrderService.cs
@@ -7,7 +7,7 @@ namespace Core.Interfaces
     public interface IOrderService
     {
         Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketId, Address shippingAddress);
-        Task<IReadOnlyList<Order>> GetOrdersForUser(string buyerEmail);
+        Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail);
         Task<Order> GetOrderByIdAsync(int id, string buyerEmail);
         Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync();
     }
